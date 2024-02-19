1f6fff060f37ab1257a722310126d6b96b693ffa
Ashhar-24 <mdashhar01@gmail.com>
2024-02-19 19:33:43 +0530

    Day 4/75 (900 rated)

---
+++ ACodeDaily_Hard75/A_Dubstep.cpp
@@ -0,0 +1,32 @@
+// Quesn: https://codeforces.com/contest/208/problem/A (900 rated)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s; cin>>s;
+
+    string ans;
+    int i=0; 
+
+    /*
+        Just loop over the string if found WUB increment the iterator by 3. Else push the character and check if the
+        next 3 characters are WUB, if yes add a space and increase the iterator by 4.
+    */
+
+    while(i<s.length()){
+        if(s[i]=='W' and s[i+1]=='U' and s[i+2]=='B'){
+            i+=3;
+        } 
+
+        else{
+            ans.push_back(s[i]);
+            if(s[i+1]=='W' and s[i+2]=='U' and s[i+3]=='B') {
+                ans.append(" ");
+                i+=4;
+            }
+            else i++;
+        }
+    }
+        cout<<ans;
+}
