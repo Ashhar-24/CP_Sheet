ee06ac4c53a3ea61cb2c72c26127964a4caf1a39
Ashhar-24 <mdashhar01@gmail.com>
2024-02-21 17:42:01 +0530

    1100_STL(Vector, Set)

--- ACodeDaily_Hard75/D_Double_Strings.cpp
+++ ACodeDaily_Hard75/D_Double_Strings.cpp
@@ -5,9 +5,62 @@ using namespace std;
 void solve(){
     int n; cin>>n;
     vector <string> v(n);
-    for(int i=0; i<n; i++) cin>>v[i];
+    unordered_set <string> s;
+    for(int i=0; i<n; i++){
+        cin>>v[i];
+        s.insert(v[i]);
+    } 
 
-    
+    string ans;
+
+   for(int i=0; i<n; i++){
+        if(v[i].size()==1){
+            ans.append("0");
+            continue;
+        }
+
+        string temps = v[i];
+        string subs="";
+
+        bool ispresent=0;
+
+        while(!temps.empty()){     
+            subs=temps[temps.length()-1]+subs;
+            temps.pop_back();
+
+            if(s.count(subs) and s.count(temps)){
+                ans.append("1");
+                ispresent=1;
+                break;
+            }
+        }
+            if(ispresent!=1)ans.append("0");
+        }
+
+    /*
+        O(n^2) approach: O(n) for loop and O(n) for find()
+
+        for(int i=0; i<n; i++){
+            string s = v[i];
+            string temp="";
+
+            bool ispresent=0;
+
+            while(!s.empty()){
+                temp=s[s.length()-1]+temp;
+                s.pop_back();
+                if(find(v.begin(), v.end(), temp)!=v.end() && find(v.begin(), v.end(), s)!=v.end()){
+                    ispresent=1;
+                }
+                if(ispresent) break; 
+            }
+            if(ispresent!=1)ans.append("0");
+            else ans.append("1");
+        }
+
+    */
+    cout<<ans<<'\n';
+
 }	
 
 
@@ -20,3 +73,5 @@ int main() {
 
     return 0;
 }
+
+// Quesn: https://codeforces.com/contest/1703/problem/D
