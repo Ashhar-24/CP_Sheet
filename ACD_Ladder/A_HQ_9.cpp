9b872923837bc13a1585a98d98cf6555bf0d8c70
Ashhar-24 <mdashhar01@gmail.com>
2024-02-22 12:43:21 +0530

    900_String

--- ACD_Ladder/A_HQ_9.cpp
+++ ACD_Ladder/A_HQ_9.cpp
@@ -15,5 +15,11 @@ int main(){
     if(present) cout<<"YES"<<'\n';
     else cout<<"NO"<<'\n';
 
-    // Quesn: https://codeforces.com/contest/133/problem/A
 }
+
+// Quesn: https://codeforces.com/contest/133/problem/A
+/*
+    Question language is bit complex, but with simple reading and observation
+    it is clear that if the input string has either of word 'H','Q' or '9', it
+    will print an output else not.
+*/
