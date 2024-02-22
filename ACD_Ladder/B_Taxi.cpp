dd9fef47dc280726da9b7cae22daf404289e946d
Ashhar-24 <mdashhar01@gmail.com>
2024-02-22 21:29:34 +0530

    1100_Greedy

--- ACD_Ladder/B_Taxi.cpp
+++ ACD_Ladder/B_Taxi.cpp
@@ -1,7 +1,3 @@
-// Quesn: https://codeforces.com/contest/158/problem/B (1100)
-
-// Basic approach but a lot of edge cases / combinations possible
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -60,8 +56,21 @@ int main(){
 
     // cout<<ans<<'\n';
 
-    if(c1>4) ans+= ceil(c1/4.0);
-    else if(c1)ans++;
+    if(c1) ans+= ceil(c1/4.0);
+    // else if(c1)ans++;
 
     cout<<ans<<'\n';
 }
+
+// Quesn: https://codeforces.com/contest/158/problem/B (1100)
+
+/*
+    Approach=> Greedy Algo (https://crackfaang.medium.com/intro-to-greedy-algorithms-with-c-546d66d30502)
+    
+        We simply count the no of 1s,2s,3s and 4s. For 4s we simply send each group
+        into taxi. Pair 3s and 1s as much as possible. Then first we pair all 2s together
+        if left, pair with left 1's (if) else send it alone. Then for all remaining 1s
+        we pair them up to 4, ie take ceil() value of 1s with 4.0
+
+*/ 
+
