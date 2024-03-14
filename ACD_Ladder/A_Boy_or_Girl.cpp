79f556ecbdc068896924b19d70c3892a38b3da0f
Ashhar-24 <mdashhar01@gmail.com>
2024-03-14 17:08:16 +0530

    800_Set_Implementation

---
+++ ACD_Ladder/A_Boy_or_Girl.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void solve(){
+    string s; cin>>s;
+
+    set<char>st (s.begin(), s.end());
+
+    if(st.size()%2==0) cout<<"CHAT WITH HER!"<<'\n';
+    else cout<<"IGNORE HIM!"<<'\n';
+    
+}	
+
+
+int main() {
+    int t=1; 
+    // cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
