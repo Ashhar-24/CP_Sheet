ba5c1ec468d81eb2a2719e43ff51df590b718758
Ashhar-24 <mdashhar01@gmail.com>
2024-03-20 23:17:48 +0530

    800_Constructive*

--- TLE_CP_31/CF (800)/B_Sequence_Game.cpp
+++ TLE_CP_31/CF (800)/B_Sequence_Game.cpp
@@ -11,11 +11,11 @@ void solve(){
     ans.push_back(v[0]);
 
     for(int i=1; i<n; i++){
-        if(v[i]<v[i-1]){
-            ans.push_back(v[i]);
+        if(v[i]>=v[i-1]){
             ans.push_back(v[i]);
         }
         else {
+            ans.push_back(v[i]);
             ans.push_back(v[i]);
         }
     }
@@ -37,3 +37,12 @@ int main() {
 }
 
 // Question: https://codeforces.com/problemset/problem/1862/B
+
+/*
+    Here after carefull observation, 1st element as given will be same. Now for next element, if v[i]>=v[i-1], ie
+    current element is either greater or equal to previous element insert it once, else twice.
+    Doing so because, if the current element is smaller to previous element then it must be present 2 times
+    (other possible elements could have been v[i]-1, v[i]-2,....1 ; but we didn't choose to insert v[i]-1 as
+    if v[i]==1 then 0 will be inserted and in the question it is not allowed, hence we inserted the same element)
+    so that the given conditions are satisfied. 
+*/
